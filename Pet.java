305b38edf66d0a4205c57aa6d43e697ca5b4d050
SunnyFB <151699530+SunnyFB@users.noreply.github.com>
2025-02-14 13:36:21 -0600

    random garbage

---
+++ Pet.java
@@ -0,0 +1,13 @@
+public class Pet {
+    private int hunger, weight, happiness, energy, health;
+    private int maxHunger, maxWeight, maxHappiness, maxEnergy, maxHealth;
+
+    public Pet(int maxHunger, int maxWeight, int maxHappiness, int maxEnergy, int maxHealth)
+    {
+        this.maxHunger = maxHunger;
+        this.maxWeight = maxWeight;
+        this.maxHappiness = maxHappiness;
+        this.maxEnergy = maxEnergy;
+        this.maxHealth = maxHealth;
+    }
+}
