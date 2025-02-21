47fd1e9d45bf503482d691ff739ba4e507aac9d5
ruth-turc <ruth.turc@gmail.com>
2025-02-21 13:34:30 -0600

    Create Rock.java
    Rock subclass of Pet

---
+++ Rock.java
@@ -0,0 +1,53 @@
+public class Rock extends Pet
+{
+    /**
+     * a subclass of Pet. The pet rock only has happiness.
+     */
+
+    public Rock(int happiness)
+    {
+        super(100,100,happiness,100,100);
+    }
+
+    public Rock()
+    {
+        super();
+    }
+
+    /**
+     * Rock's version of walk
+     */
+    public void walk()
+    {
+        super.setHappiness(super.getHappiness() + 1);
+        super.boundValues();
+    }
+
+    /**
+     * rock's version of play
+     */
+    public void play()
+    {
+        super.setHappiness(super.getHappiness() + 2);
+        super.boundValues();
+    }
+
+    /**
+     * rock's version of taking to doctor
+     */
+    public void toDoctor()
+    {
+        super.setHappiness(super.getHappiness() - 1);
+        super.boundValues();
+    }
+
+    /**
+     * rock's version on training
+     */
+    public void train()
+    {
+        super.setHappiness(super.getHappiness()+1);
+        super.boundValues();
+    }
+
+}
