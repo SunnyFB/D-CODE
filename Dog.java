44fbd1d79792cb9805ba7e9caa1abb1f60e083ed
ruth-turc <ruth.turc@gmail.com>
2025-02-21 13:35:23 -0600

    Create Dog()

---
+++ Dog.java
@@ -0,0 +1,48 @@
+public class Dog extends Pet
+{
+    /**
+     * Dog is a subclass of Pet. 
+     * Dogs need more attention, and its happiness will run out faster.
+     */
+    
+    private final int maxHappiness = 50;
+
+    public Dog(double hunger,int weight, int happiness,int energy,int health)
+    {
+        super(hunger,weight,happiness,energy,health);
+    }
+
+    public Dog()
+    {
+        super();
+    }
+
+    public void boundValues()
+    {
+        super.boundValues();
+        if (super.getHappiness() > maxHappiness)
+        {
+            super.setHappiness(maxHappiness);
+        }
+    }
+
+    public void walk()
+    {
+
+    }
+
+    public void play()
+    {
+
+    }
+
+    public void toDoctor()
+    {
+
+    }
+
+    public void train()
+    {
+
+    }
+}
