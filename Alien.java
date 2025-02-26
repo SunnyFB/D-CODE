769a661a896c2bccf89f26f4735b9b22203dbe7a
ruth-turc <ruth.turc@gmail.com>
2025-02-26 13:01:10 -0600

    Update Alien.java

--- Alien.java
+++ Alien.java
@@ -1,3 +1,16 @@
-public class Alien {
+public class Alien extends Pet{
 
+    /**
+     * Alien is a subclass of Pet.
+     * Aliens are weird, and its stats will change differently when it does an activity.
+     */
+    public Alien(double hunger,double weight, double happiness,double energy,double health)
+    {
+        super(hunger,weight,happiness,energy,health);
+    }
+
+    public Alien()
+    {
+        super();
+    }
 }
