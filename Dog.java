d32f91d98c332c8196854d3ede28e6161c9b2149
ruth-turc <ruth.turc@gmail.com>
2025-03-04 11:36:44 -0600

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -2,7 +2,7 @@ public class Dog extends Pet
 {
     /**
      * Dog is a subclass of Pet. 
-     * Dogs need more attention, and get less happiness from certain things.
+     * Dogs need more attention, and lose happiness faster.
      */
     public Dog(double fullness,double weight, double happiness,double energy,double health)
     {
@@ -14,44 +14,8 @@ public class Dog extends Pet
         super();
     }
 
-    /**
-     * Dog's version of walk.
-     * Dogs gain less happiness from walk.
-     */
-    public void walk()
+    public double HappinessDrain()
     {
-        super.setHappiness(super.getHappiness() - 0.5);
-        super.walk();
-    }
-
-    /**
-     * Dog's version of play.
-     * Dogs gain less happiness from play.
-     */
-    public void play()
-    {
-        super.setHappiness(super.getHappiness() - 1);
-        super.play();
-    }
-
-    /**
-     * Dog's version of train.
-     * Dog's gain less happiness from train.
-     */
-    public void train()
-    {
-        super.setHappiness(super.getHappiness() - 0.5);
-        super.train();
-
-    }
-
-    /**
-     * Dog's version of going to the doctor.
-     * Dogs get slightly more upset from going to the doctor.
-     */
-    public void toDoctor()
-    {
-        super.setHappiness(super.getHappiness() - 0.5);
-        super.toDoctor();
+        return 0.1;
     }
 }
