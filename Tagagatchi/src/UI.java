72e729774650fec8cde6979f5d61e6a3c96f46ea
ruth-turc <ruth.turc@gmail.com>
2025-04-01 13:23:22 -0500

    Update UI.java
    added the rest of pet attributes

--- Tagagatchi/src/UI.java
+++ Tagagatchi/src/UI.java
@@ -8,10 +8,14 @@ public class UI extends Game{
     JTextField fullnessField;
     JTextField healthField;
     JTextField weightField;
+    JTextField hygieneField;
+    JTextField energyField;
+    JTextField happinessField;
 
     //variables to update Game
     public boolean feedFood = false;
     public boolean feedTreat = false;
+    public boolean play = false;
     public boolean save = false, paused = false;
 
     public String petType;
@@ -26,6 +30,7 @@ public class UI extends Game{
         JPanel panel = new JPanel();
 
         JButton feedButton = new JButton("Feed");
+        JButton playButton = new JButton("Play");
 
 
         fullnessField = new JTextField("Fullness: " + vPet.getFullness());
@@ -37,6 +42,15 @@ public class UI extends Game{
         weightField = new JTextField("Health: " + vPet.getFullness());
         weightField.setEditable(false);
 
+        hygieneField = new JTextField("Hygiene: " + vPet.getHygiene());
+        hygieneField.setEditable(false);
+
+        energyField = new JTextField("Energy: " + vPet.getEnergy());
+        energyField.setEditable(false);
+
+        happinessField = new JTextField("Happiness: " + vPet.getHappiness());
+        happinessField.setEditable(false);
+
         ActionListener a1 = new ActionListener() {
             public void actionPerformed(ActionEvent ae){
                 if (!paused)
@@ -44,6 +58,14 @@ public class UI extends Game{
             }
         };
         feedButton.addActionListener(a1);
+
+        ActionListener a4 = new ActionListener(){
+            public void actionPerformed(ActionEvent ae) {
+                if (!paused)
+                    play = true;
+            }
+        };
+        playButton.addActionListener(a4);
 
         JButton saveButton = new JButton("Save");
         ActionListener a2 = new ActionListener() {
@@ -62,10 +84,19 @@ public class UI extends Game{
         };
         pauseButton.addActionListener(a3);
 
+        //activity buttons
         panel.add(feedButton);
+        panel.add(playButton);
+
+        //pet attributes
         panel.add(fullnessField);
         panel.add(healthField);
         panel.add(weightField);
+        panel.add(hygieneField);
+        panel.add(energyField);
+        panel.add(happinessField);
+
+        //save and pause button
         panel.add(saveButton);
         panel.add(pauseButton);
         frame.add(panel);
